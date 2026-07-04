2a702c16f2acca6dd1bc33df347cb4edb0d37c42
ayush-cg-code <ayush.arvind-bedre@capgemini.com>
2026-07-04 22:27:29 +0530

    Configure Render Key Value in render client

--- services/redis_client.py
+++ services/redis_client.py
@@ -7,6 +7,7 @@ from config import settings
 redis_client = redis.Redis(
     host=settings.REDIS_HOST,
     port=settings.REDIS_PORT,
+    password=settings.REDIS_PASSWORD,
     db=settings.REDIS_DB,
     decode_responses=True
 )
